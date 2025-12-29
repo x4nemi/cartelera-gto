81c5ede64b1c5ace920123ef80865facc1e877fe
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2025-12-28 19:50:04 -0600

    feat: refactor UserPage layout for improved structure and styling

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -19,55 +19,57 @@ export const UserPage = () => {
     };
     return (
         <DefaultLayout>
-            <Card className='p-5'>
-                <CardHeader>
-                    <h4 className="font-bold text-xl">Crea tu usuario</h4>
-                </CardHeader>
-                <CardBody>
-                    <Form
-                        className="w-full flex flex-col gap-3"
-                        validationErrors={errors}
-                        onSubmit={onSubmit}
-                    >
-                        <label htmlFor="username" className='text-lg'>Tu usuario de Instagram</label>
-                        <div className='flex w-full'>
-                            <Input
-                                size="lg"
-                                labelPlacement="outside"
-                                name="username"
-                                classNames={{
-                                    inputWrapper: "h-12 rounded-r-none",
-                                }}
-                                placeholder="tu_usuario"
-                                startContent={
-                                    <div className="pointer-events-none flex items-center">
-                                        <span className="text-small text-pink-300">@</span>
-                                    </div>
-                                }
-                            />
-                            <Button type="submit" variant="flat" size='lg' className="w-32 h-12 rounded-l-none bg-pink-400 text-white">
-                                Siguiente
-                            </Button>
-                        </div>
-                    </Form>
-                    <Card shadow='sm' className='mt-5 rounded-xl'>
-                       <Alert hideIcon color="success" description='' title="Usuario encontrado" variant="faded" className='rounded-b-none p-0 pl-4' startContent={<CheckIcon size={15} />} />
-                        <CardBody>
-                            <User
-                                avatarProps={{
-                                    src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-                                }}
-                                description={
-                                    <Link isExternal href="https://x.com/jrgarciadev" size="sm">
-                                        @jrgarciadev
-                                    </Link>
-                                }
-                                name="Junior Garcia"
-                            />
-                        </CardBody>
-                    </Card>
-                </CardBody>
-            </Card>
+            <div className='flex w-full justify-center'>
+                <Card className='p-5 md:w-lg w-full'>
+                    <CardHeader>
+                        <h4 className="font-bold text-xl">Crea tu usuario</h4>
+                    </CardHeader>
+                    <CardBody>
+                        <Form
+                            className="w-full flex flex-col gap-3"
+                            validationErrors={errors}
+                            onSubmit={onSubmit}
+                        >
+                            <label htmlFor="username" className='text-lg'>Tu usuario de Instagram</label>
+                            <div className='flex w-full'>
+                                <Input
+                                    size="lg"
+                                    labelPlacement="outside"
+                                    name="username"
+                                    classNames={{
+                                        inputWrapper: "h-12 rounded-r-none",
+                                    }}
+                                    placeholder="tu_usuario"
+                                    startContent={
+                                        <div className="pointer-events-none flex items-center">
+                                            <span className="text-small text-pink-300">@</span>
+                                        </div>
+                                    }
+                                />
+                                <Button type="submit" variant="flat" size='lg' className="w-32 h-12 rounded-l-none bg-pink-400 text-white">
+                                    Siguiente
+                                </Button>
+                            </div>
+                        </Form>
+                        <Card shadow='sm' className='mt-5 rounded-xl'>
+                            <Alert hideIcon color="success" description='' title="Usuario encontrado" variant="faded" className='rounded-b-none p-0 pl-4' startContent={<CheckIcon size={15} />} />
+                            <CardBody>
+                                <User
+                                    avatarProps={{
+                                        src: "https://avatars.githubusercontent.com/u/30373425?v=4",
+                                    }}
+                                    description={
+                                        <Link isExternal href="https://x.com/jrgarciadev" size="sm">
+                                            @jrgarciadev
+                                        </Link>
+                                    }
+                                    name="Junior Garcia"
+                                />
+                            </CardBody>
+                        </Card>
+                    </CardBody>
+                </Card>
+            </div>
         </DefaultLayout>
     )
 }
