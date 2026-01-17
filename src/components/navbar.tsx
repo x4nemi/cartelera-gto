20960d9f6caf52485c64f1f870a5f0b29665160b
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2026-01-16 22:02:33 -0600

    feat: add ViewSwitch component for toggling between calendar and event views in Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -19,9 +19,11 @@ import {
 } from "@/components/icons";
 import { Logo } from "@/components/icons";
 import { useState } from "react";
+import { ViewSwitch } from "./viewSwitch";
 
 export const Navbar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const path = typeof window !== 'undefined' ? window.location.pathname : '/';
 	return (
 		<HeroUINavbar maxWidth="xl" position="sticky" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -58,6 +60,11 @@ export const Navbar = () => {
 				className="hidden sm:flex basis-1/5 sm:basis-full"
 				justify="end"
 			>
+				{path === "/" &&
+					<NavbarItem className="flex gap-2">
+						<ViewSwitch />
+					</NavbarItem>
+				}
 				<NavbarItem className="hidden sm:flex gap-2">
 					<ThemeSwitch />
 				</NavbarItem>
@@ -66,7 +73,7 @@ export const Navbar = () => {
 						isExternal
 						as={Link}
 						className="text-sm font-normal text-default-600 bg-default-100"
-						href={siteConfig.links.sponsor}
+						// href={siteConfig.links.sponsor}
 						startContent={<HeartFilledIcon className="text-danger" />}
 						variant="flat"
 					>
@@ -76,6 +83,7 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+				<ViewSwitch />
 				<ThemeSwitch />
 			</NavbarContent>
 
---
+++ src/components/viewSwitch.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@heroui/button';
+import { useEffect, useState } from 'react'
+import { CalendarIcon, EventIcon } from './icons';
+
+export const ViewSwitch = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    const [isSelected, setIsSelected] = useState(false)
+
+    const handleSelect = () => {
+        localStorage.setItem('viewMode', isSelected ? 'calendar' : 'events')
+        setIsSelected(!isSelected)
+    }
+
+    useEffect(() => {
+        // if localStorage does not have viewMode, set to events
+        const storedViewMode = localStorage.getItem('viewMode');
+        if (storedViewMode) {
+            setIsSelected(storedViewMode === 'events' ? false : true)
+        } else {
+            localStorage.setItem('viewMode', 'events')
+            setIsSelected(false)
+        }
+        setIsMounted(true);
+    }, [isMounted]);
+    
+    if (!isMounted) return <div className="w-6 h-6" />;
+    
+  return (
+     <div className="flex flex-col gap-2">
+        <Button variant='bordered' color='secondary' className='mx-auto' startContent={isSelected ? <CalendarIcon /> : <EventIcon />} onPress={handleSelect}>{isSelected ? "Calendario" : "Eventos"}</Button>
+    </div>
+  )
+}
