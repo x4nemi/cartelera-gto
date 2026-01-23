3f7d1eb4d294e8900494e61529339675040a4c79
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2026-01-22 21:35:17 -0600

    fix: update CustomRadio styles for improved appearance and icon sizes

--- src/components/radioGroup.tsx
+++ src/components/radioGroup.tsx
@@ -9,8 +9,8 @@ export const CustomRadio = ({ children, ...props }: & React.ComponentProps<typeo
             classNames={{
                 base: cn(
                     "max-w-full inline-flex m-0 bg-content1 hover:bg-content2 items-center justify-between",
-                    "cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
-                    "data-[selected=true]:border-secondary border-default justify-center text-center data-[selected=true]:bg-violet-100 data-[selected=true]:dark:bg-violet-800/20",
+                    "cursor-pointer rounded-2xl gap-2 p-4 border-2 border-transparent",
+                    "data-[selected=true]:border-secondary border-default justify-center text-center data-[selected=true]:bg-violet-100 data-[selected=true]:dark:bg-violet-800/20 transition-all duration-200",
                 ),
                 wrapper: "hidden",
                 description: "text-sm text-default-400 mt-1",
@@ -29,7 +29,7 @@ const CustomRadioGroup = () => {
             <div className="flex flex-row gap-4 w-full">
                 <CustomRadio description="Es un evento o un taller" value="Evento" className="flex-1" >
                     <div className="flex flex-col justify-center items-center gap-1">
-                        {value !== "Evento" ? <EventIcon size={24} color="bg-secondary" /> : <EventFilledIcon size={24} />}
+                        {value !== "Evento" ? <EventIcon size={26} color="bg-secondary" /> : <EventFilledIcon size={26} />}
                         <p>
                             Evento
                         </p>
@@ -37,7 +37,7 @@ const CustomRadioGroup = () => {
                 </CustomRadio>
                 <CustomRadio description="Contiene varios eventos" value="Calendario" className="flex-1" >
                     <div className="flex flex-col justify-center items-center gap-1">
-                        {value !== "Calendario" ? <CalendarIcon size={24} color="bg-secondary" /> : <CalendarFilledIcon size={24} />}
+                        {value !== "Calendario" ? <CalendarIcon size={26} color="bg-secondary" /> : <CalendarFilledIcon size={26} />}
                         <p>
                             Calendario
                         </p>
