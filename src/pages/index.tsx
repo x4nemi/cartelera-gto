021211cd85d0c1d9c5e6b0f5f3e9184e3f705eef
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2026-02-07 21:32:18 -0600

    style: adjust layout and spacing in DefaultLayout and PublishPage for improved visual consistency

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,39 +17,10 @@ export default function IndexPage() {
 			return isAscendingOrder ? dateA - dateB : dateB - dateA;
 		});
 	}, [isAscendingOrder]);
-
-	// const splitEventsByMonth = (events: PostData[], month: string) => {
-	// 	return events.filter((event) => {
-	// 		const eventDate = new Date(// get the first date after today if exists, else the first date
-	// 			event.dates && event.dates.length > 0
-	// 				? Math.min(
-	// 					...event.dates.map(date => new Date(date.year, date.month - 1, date.day).getTime())
-	// 				)
-	// 				: new Date().getTime()
-	// 		);
-	// 		const eventMonthYear = eventDate.toLocaleString('es-MX', { month: 'long', year: 'numeric' });
-	// 		return eventMonthYear === month;
-	// 	});
-	// }
-
-	// const months = useMemo(() => {
-	// 	const monthsSet = new Set<string>();
-	// 	orderedEvents.forEach((event : PostData) => {
-	// 		const eventDate = new Date(// get the first date after today if exists, else the first date
-	// 			event.dates && event.dates.length > 0
-	// 				? Math.min(
-	// 					...event.dates.map(date => new Date(date.year, date.month - 1, date.day).getTime())
-	// 				)
-	// 				: new Date().getTime()
-	// 		);
-	// 		const monthYear = eventDate.toLocaleString('es-MX', { month: 'long', year: 'numeric' });
-	// 		monthsSet.add(monthYear);
-	// 	});
-	// 	return Array.from(monthsSet);
-	// }, [orderedEvents]);
+	
 	return (
 		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center md:gap-4 gap-2 -mt-10 relative mx-2">
+			<section className="flex flex-col items-center justify-center md:gap-4 gap-2 mt-5 relative mx-2">
 				<div className="container mx-auto justify-center transition-all duration-300">
 					<div>
 						<FilterWidget isAscending={isAscendingOrder} setIsAscending={setIsAscendingOrder}/>
--- src/pages/publish.tsx
+++ src/pages/publish.tsx
@@ -118,135 +118,130 @@ export default function PublishPage() {
 
 	return (
 		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-4 -mt-10">
-				<div className="flex flex-col w-full max-w-3xl px-2">
-					<h1 className={title({ class: "mb-4 flex items-center gap-2 text-3xl" })}>
-						<span className="relative flex size-3">
-							<span className={`absolute inline-flex h-full w-full animate-ping rounded-full opacity-75 ${isLinkValid ? "bg-amber-400" : "bg-sky-400"}`}></span>
-							<span className={`relative inline-flex size-3 rounded-full ${isLinkValid ? "bg-amber-500" : "bg-sky-500"}`}></span>
-						</span>
-						Crea tu publicación</h1>
-					<h3 className="font-medium text-foreground mt-6 mb-2 text-lg">¿Cómo deseas publicarlo?</h3>
-					<div className="w-full">
-						<Accordion
-							showDivider={false}
-							selectionMode="single"
-							hideIndicator
-							as={"div"}
-							selectedKeys={selectedKey ? [selectedKey] : []}
-							onSelectionChange={(keys) => {
-								const key = Array.from(keys)[0] as string;
-								setSelectedKey(key === selectedKey ? key : key);
-							}}
-							style={{ paddingLeft: 0, paddingRight: 0 }}
-							itemClasses={{
-								base: "mb-2",
-								trigger: cn(
-									"p-4 bg-background/70 hover:bg-content2 border-2 border-primary-500 rounded-2xl hover:border-primary-400",
-									" data-[open=true]:rounded-b-none data-[open=true]:bg-gray",
-									"flex items-center justify-between gap-4 duration-250 transition-all backdrop-blur-md"
-								),
-								content: "px-4 py-4 bg-gray border-2 border-t-0  rounded-b-2xl bg-background/70 backdrop-blur-md border-primary-500",
-							}}
-						>
-							<AccordionItem
-								key="1"
-								aria-label="Free"
-								title={
-									<div className="flex items-center justify-between w-full">
-										<div className="flex flex-col items-start">
-											<span className="font-bold text-md">Con un link de Instagram</span>
-											<span className="text-default-400 font-medium text-sm italic">Extrae las imágenes y detalles de tu evento</span>
-										</div>
+			<section className={`flex flex-col justify-center gap-4 flex-grow max-w-3xl w-full mx-auto px-2" + ${selectedKey ? " mt-10" : " mt-20"}`}>
+				<h1 className={title({ class: "mb-2 flex items-center gap-2 text-2xl"})}>
+					<span className="relative flex size-3">
+						<span className={`absolute inline-flex h-full w-full animate-ping rounded-full opacity-75 ${isLinkValid ? "bg-amber-400" : "bg-sky-400"}`}></span>
+						<span className={`relative inline-flex size-3 rounded-full ${isLinkValid ? "bg-amber-500" : "bg-sky-500"}`}></span>
+					</span>
+					Crea tu publicación</h1>
+				<h3 className="font-semibold text-foreground mt-1 text-lg">¿Cómo deseas publicarlo?</h3>
+				<div className="w-full">
+					<Accordion
+						showDivider={false}
+						selectionMode="single"
+						hideIndicator
+						as={"div"}
+						selectedKeys={selectedKey ? [selectedKey] : []}
+						onSelectionChange={(keys) => {
+							const key = Array.from(keys)[0] as string;
+							setSelectedKey(key === selectedKey ? key : key);
+						}}
+						style={{ paddingLeft: 0, paddingRight: 0 }}
+						itemClasses={{
+							base: "mb-2",
+							trigger: cn(
+								"p-4 bg-content2/70 rounded-2xl hover:bg-primary-200/70",
+								"data-[open=true]:rounded-b-none data-[open=true]:bg-primary-200/70 data-[open=true]:border-primary-300",
+								"flex items-center justify-between gap-4 duration-250 transition-all backdrop-blur-md"
+							),
+							content: "px-4 py-4 bg-gray border-t-0 rounded-b-2xl bg-content2/70 backdrop-blur-md",
+						}}
+					>
+						<AccordionItem
+							key="1"
+							aria-label="Free"
+							title={
+								<div className="flex items-center justify-between w-full">
+									<div className="flex flex-col items-start">
+										<span className="font-bold text-md">Con un link de Instagram</span>
+										<span className="text-default-600 font-medium text-sm italic">Extrae las imágenes y detalles de tu evento</span>
 									</div>
-								}
-							>
-								<div className="flex flex-col gap-2">
-									<label htmlFor="instagram-link" className="text-sm font-medium text-foreground">Link de la publicación</label>
-									<div className="flex">
-										<Input
-											id="instagram-link"
-											placeholder="https://www.instagram.com/p/..."
-											value={link}
-											onChange={(e) => setLink(e.target.value)}
-											classNames={{
-												input: "text-sm",
-												inputWrapper: `h-12 ${isLinkValid ? "rounded-r-none text-default-400" : ""}`,
-											}}
-											isClearable={isLinkValid}
-											onClear={() => { setIsValidateButtonClicked(false); setLink("") }}
-											ref={IgInputRef}
-											readOnly={isLinkValid}
-										/>
-										{isLinkValid && <Button
-											color="primary"
-											variant="flat"
-											isLoading={loading}
-											isDisabled={link.length === 0}
-											onPress={() => handleValidatingLink(link)}
-											className="min-w-24 h-12 rounded-l-none"
-										>
-											Validar
-										</Button>}
-									</div>
-									{isValidateButtonClicked && link.length > 0 && !loading && !isLinkValid && (
-										<p className="text-xs text-danger">Link inválido. Inténtalo de nuevo.</p>
-									)}
-
-									{isLinkValid && (
-										<div className="mt-2 flex flex-col rounded-xl ">
-											<p className="text-sm font-medium text-foreground-500 mb-2">Publicación encontrada</p>
-
-											<Card className="flex flex-col justify-start mb-2 bg-content2 p-4 gap-2" shadow="none">
-												<p className="text-small text-foreground-700  mb-2">Usuario</p>
-												<User
-													avatarProps={{
-														src: postData?.ownerProfilePicUrl || "",
-													}}
-													description={
-														<Link isExternal href={"https://www.instagram.com/" + postData?.ownerUsername} size="sm">
-															@{postData?.ownerUsername}
-														</Link>
-													}
-													className="self-start ml-3"
-													name={postData?.ownerFullName || ""}
-												/>
-												{/* </div> */}
-												<Textarea className="w-full mt-3" label="Descripción" placeholder="Describe tu evento aquí" value={postData?.caption || ""} labelPlacement="outside" variant="bordered" />
-
-												<p className="text-small text-foreground-700 mt-3 mb-1">Imágenes</p>
-												<div className="gap-2 flex flex-col">
-													<ImageGallery images={postData?.images?.map(url => ({ src: url })) || (postData?.displayUrl ? [{ src: postData.displayUrl }] : [])} />
-												</div>
-											</Card>
-											<DatesWidget selectedDays={selectedDates} onChange={setSelectedDates} />
-
-											{/* <ButtonGroup> */}
-
-										</div>
-									)}
 								</div>
-							</AccordionItem>
-							<AccordionItem
-								key="2"
-								aria-label="Pro"
-								title={
-									<div className="flex items-center justify-between w-full">
-										<div className="flex flex-col items-start ">
-											<span className="font-bold text-md">Desde cero</span>
-											<span className="text-default-400 font-medium text-sm italic">Sube tus propias imágenes y detalles</span>
-										</div>
+							}
+							classNames={{base:"backdrop-blur-sm mb-2 "}}
+						>
+							<div className="flex flex-col gap-2">
+								<label htmlFor="instagram-link" className="text-sm font-medium text-foreground">Link de la publicación</label>
+								<div className="flex">
+									<Input
+										id="instagram-link"
+										placeholder="https://www.instagram.com/p/..."
+										value={link}
+										onChange={(e) => setLink(e.target.value)}
+										classNames={{
+											input: "text-sm",
+											inputWrapper: `h-12 ${isLinkValid ? "rounded-r-none text-default-400" : ""}`,
+										}}
+										isClearable={isLinkValid}
+										onClear={() => { setIsValidateButtonClicked(false); setLink("") }}
+										ref={IgInputRef}
+										readOnly={isLinkValid}
+									/>
+									{isLinkValid && <Button
+										color="primary"
+										variant="flat"
+										isLoading={loading}
+										isDisabled={link.length === 0}
+										onPress={() => handleValidatingLink(link)}
+										className="min-w-24 h-12 rounded-l-none"
+									>
+										Validar
+									</Button>}
+								</div>
+								{isValidateButtonClicked && link.length > 0 && !loading && !isLinkValid && (
+									<p className="text-xs text-danger">Link inválido. Inténtalo de nuevo.</p>
+								)}
+
+								{isLinkValid && (
+									<div className="mt-2 flex flex-col rounded-xl ">
+										<p className="text-sm font-medium text-foreground-500 mb-2">Publicación encontrada</p>
+
+										<Card className="flex flex-col justify-start mb-2 bg-content2 p-4 gap-2" shadow="none">
+											<p className="text-small text-foreground-700  mb-2">Usuario</p>
+											<User
+												avatarProps={{
+													src: postData?.ownerProfilePicUrl || "",
+												}}
+												description={
+													<Link isExternal href={"https://www.instagram.com/" + postData?.ownerUsername} size="sm">
+														@{postData?.ownerUsername}
+													</Link>
+												}
+												className="self-start ml-3"
+												name={postData?.ownerFullName || ""}
+											/>
+											<Textarea className="w-full mt-3" label="Descripción" placeholder="Describe tu evento aquí" value={postData?.caption || ""} labelPlacement="outside" variant="bordered" />
+
+											<p className="text-small text-foreground-700 mt-3 mb-1">Imágenes</p>
+											<div className="gap-2 flex flex-col">
+												<ImageGallery images={postData?.images?.map(url => ({ src: url })) || (postData?.displayUrl ? [{ src: postData.displayUrl }] : [])} />
+											</div>
+										</Card>
+										<DatesWidget selectedDays={selectedDates} onChange={setSelectedDates} />
 									</div>
-								}
-								className="mb-0"
-							>
-								<FileUploadButton />
-							</AccordionItem>
-						</Accordion>
-						<div className="flex w-full justify-end gap-2 mt-3">
-							<Button size="lg" color="danger" variant="bordered">Cancelar</Button>
-							<Button className="" size="lg" color="primary" variant="solid" onPress={handlePublishPost}>Crear evento</Button>
-						</div>
+								)}
+							</div>
+						</AccordionItem>
+						<AccordionItem
+							key="2"
+							aria-label="Pro"
+							title={
+								<div className="flex items-center justify-between w-full">
+									<div className="flex flex-col items-start ">
+										<span className="font-bold text-md">Desde cero</span>
+										<span className="text-default-700 font-medium text-sm italic">Sube tus propias imágenes y detalles</span>
+									</div>
+								</div>
+							}
+							className="mb-0"
+						>
+							<FileUploadButton />
+						</AccordionItem>
+					</Accordion>
+					<div className="flex w-full justify-end gap-2 mt-3">
+						<Button size="lg" color="danger" variant="bordered">Cancelar</Button>
+						<Button className="" size="lg" color="primary" variant="solid" onPress={handlePublishPost}>Crear evento</Button>
 					</div>
 				</div>
 			</section>
